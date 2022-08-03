8b6886e242ef8e844b96740e1e0174878bbf98c0
Arthur <artyruakobhuk@gmail.com>
2022-08-03 17:16:56 +0300

    add comics and characters lists

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,81 +1,87 @@
-import './charList.scss';
-import MarvelService from '../services/MarvelService';
 import { Component } from 'react/cjs/react.development';
-import Spinner from '../spinner/Spinner';
+import MarvelService from '../services/MarvelService';
 import ErrorMessage from '../errorMessage/errorMessage';
-
+import Spinner from '../spinner/Spinner';
+import './charList.scss';
 
 class CharList extends Component {
- state = {
-	CharList : [],
-	loading: true,
-	error: false,
- }
 
-marvelService = new MarvelService();
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+    
+    marvelService = new MarvelService();
 
-updateChar = () => {
-	this.marvelService.getAllCharacters()
-		.then(this.onCharLoaded)
-		.catch(this.onError)
-}
+    componentDidMount() {
+        this.marvelService.getAllCharacters()
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
 
-onError = () => {
-	this.setState({
-		 error: true,
-		 loading: false
-	})
-}
+    onCharListLoaded = (charList) => {
+        this.setState({
+            charList,
+            loading: false
+        })
+    }
 
-onCharLoaded = (CharList) => {
-	this.setState({ CharList , loading: false })
-}
-componentDidMount() {
-	this.updateChar();
-}
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
 
-renderCharList = (arr) => {
-	const items =  arr.map((item) => {
-		let imgStyle = {'objectFit' : 'cover'};
-		if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-			 imgStyle = {'objectFit' : 'unset'};
-		}
-		
-		return (
-			 <li 
-				  className="char__item"
-				  key={item.id}>
-						<img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-						<div className="char__name">{item.name}</div>
-			 </li>
-		)
-  });
-  return (
-		<ul className="char__grid">
-			 {items}
-		</ul>
-  )
-}
+    // Этот метод создан для оптимизации, 
+    // чтобы не помещать такую конструкцию в метод render
+    renderItems(arr) {
+        const items =  arr.map((item) => {
+            let imgStyle = {'objectFit' : 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            
+            return (
+                <li 
+                    className="char__item"
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+        // А эта конструкция вынесена для центровки спиннера/ошибки
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
 
+    render() {
 
-render() {
-	const {CharList, loading, error} = this.state;
-	const items = this.renderCharList(CharList);
-	const errorMessage = error ? <ErrorMessage />: null;
-        const spinner = loading ? <Spinner /> : null;
+        const {charList, loading, error} = this.state;
+        
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? items : null;
-	return (
-		<div className="char__list">
-			  	{errorMessage}
+
+        return (
+            <div className="char__list">
+                {errorMessage}
                 {spinner}
                 {content}
-			<button className="button button__main button__long">
-				<div className="inner">load more</div>
-			</button>
-		</div>
-	)
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
 }
 
-
-}
 export default CharList;
