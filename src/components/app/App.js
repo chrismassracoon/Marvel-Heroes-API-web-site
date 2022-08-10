1a2a0cafab994cafb044cbd6f7bbf80bf0ef8f09
Arthur <artyruakobhuk@gmail.com>
2022-08-10 15:35:21 +0300

    add React Router to project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,46 +1,25 @@
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-
-import decoration from '../../resources/img/vision.png';
-import { Component } from "react/cjs/react.production.min";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import { useState } from "react";
-import ComicsList from '../comicsList/ComicsList'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ComicsPage, MainPage } from "../pages";
 
 const App = () => {
-	const [renderProp, setRenderProp] = useState('comics');
-	const	[selectedChar, setSelectedChar] = useState(null);
-
-	const onChangeRender = (prop) => {
-		setRenderProp(prop);
-	}
-
-	const onCharSelected = (id) => {
-		setSelectedChar(id);
-	}
-    return (
-        <div className="app">
-            <AppHeader renderProp={renderProp} onChangeRender={onChangeRender}/>
-            <main>
-					<ErrorBoundary>
-                <RandomChar/>
-					 </ErrorBoundary>
-					 {renderProp == 'comics' ? <ComicsList></ComicsList> : <>
-					 <div className="char__content">
-					 <ErrorBoundary>
-                    <CharList onCharSelected={onCharSelected}/>
-						  </ErrorBoundary>
-						  <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-						  </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-					 </>}
-            </main>
-        </div>
-    )
+	return (
+		<Router>
+			<div className="app">
+				<AppHeader />
+				<main>
+				<Switch>
+					<Route exact path='/'>
+						<MainPage></MainPage>
+						</Route>
+						<Route exact path='/comics'>
+						<ComicsPage></ComicsPage>
+						</Route>
+				</Switch>
+				</main>
+			</div>
+		</Router>
+	)
 }
 
 export default App;
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,14 @@
+import ComicsList from '../comicsList/ComicsList'
+import SingleComic from "../singleComic/SingleComic";
+
+const ComicsPage = () => {
+
+	return(
+		<>
+						<ComicsList />
+						<SingleComic></SingleComic>
+		</>
+	)
+}
+
+export default ComicsPage;
